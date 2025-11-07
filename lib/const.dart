cdf3ea84e41562597fc88a113634d3b54b5186d4
manamungkinn <151128430+manamungkinn@users.noreply.github.com>
2025-11-07 15:36:41 +0700

    first commit

---
+++ lib/const.dart
@@ -0,0 +1,14 @@
+List<Map<String, dynamic>> Chat = [
+  {
+    "sender": true,
+    "data": "hii bro",
+  },
+  {
+    "sender": false,
+    "data": "Ya broo",
+  },
+  {
+    "sender": true,
+    "data": "gajadi",
+  },
+];
